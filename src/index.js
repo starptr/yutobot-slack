cca9cd070210a319c872d117849ddca498c02c53
Yuto Nishida <yuyu.nishida@gmail.com>
2020-08-18 16:12:21 -0700

    Move fancyLog to another file

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 require("dotenv").config();
 const fs = require("fs").promises;
 const axios = require("axios").default;
+const { fancyLog, fancyLogConfig } = require("./fancyLog");
 const { App } = require("@slack/bolt");
 
 const app = new App({
@@ -8,35 +9,10 @@ const app = new App({
 	signingSecret: process.env.SLACK_SIGNING_SECRET,
 });
 
+fancyLogConfig(app);
+
 const unicodeCounterStart = require("./modules/unicode-counter");
 const wiggler = require("./modules/wiggler");
-
-const fancyLog = (msg, channel = process.env.SLACK_LOG_CHANNEL) => {
-	if (msg.message) console.error(msg);
-	else console.log(msg);
-
-	app.client.chat.postMessage({
-		token: process.env.SLACK_BOT_TOKEN,
-		//ID or name
-		channel,
-		text: null,
-		attachments: [
-			{
-				color: msg.message ? "#ff0000" : "#dddddd",
-				blocks: [
-					{
-						type: "section",
-						text: {
-							type: "plain_text",
-							text: `${msg.message || msg}`,
-							emoji: false,
-						},
-					},
-				],
-			},
-		],
-	});
-};
 
 // Listens to incoming messages that contain "hello"
 app.message("!ping", async ({ message, say }) => {
