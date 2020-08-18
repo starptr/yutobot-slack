44b27c40f1f11d5efe881151cfffd6dfcb86acec
Yuto Nishida <yuyu.nishida@gmail.com>
2020-08-18 16:13:11 -0700

    Added Wiggler Night mode & rare rotation

--- src/modules/wiggler/index.js
+++ src/modules/wiggler/index.js
@@ -1,36 +1,23 @@
 const _ = require("lodash");
 const axios = require("axios").default;
+const { fancyLog } = require("../../fancyLog");
 
-const wiggler = async (app) => {
-	let pfpsUrls = [
-		"http://cloud-2to2o6psz.vercel.app/cartoony1.png",
-		"http://cloud-2to2o6psz.vercel.app/cartoony2.png",
-		"http://cloud-48towm6hx.vercel.app/cartoony3_b.png",
-		"http://cloud-2to2o6psz.vercel.app/cartoony4.png",
-		"http://cloud-48towm6hx.vercel.app/cartoony5_b.png",
-		"http://cloud-2to2o6psz.vercel.app/cartoony6.png",
-		"http://cloud-2to2o6psz.vercel.app/cartoony7.png",
-		"http://cloud-2to2o6psz.vercel.app/cartoony8.png",
-	];
+const getLocalDateNow = () => new Date(new Date().toLocaleString("en-US", { timeZone: "America/Los_Angeles" }));
+
+const wiggler = async app => {
+	// Get all urls
+	let pfpsUrlsAll = require("./pfpsUrls.json");
 
 	// Last minute of execution
-	let m = new Date().getMinutes() - 1;
+	let m = getLocalDateNow().getMinutes() - 1;
 	// Last pfp's index
 	let oldIndex = -1;
 
-	// Repeated func
-	const randomPushPfp = async () => {
-		// Choose an index not equal to the last index (this algo makes it impossible to choose last pfp on first iteration)
-		let i = oldIndex + 1;
-		if (i >= pfpsUrls.length) {
-			i = 0;
-			pfpsUrls = _.shuffle(pfpsUrls);
-		}
-		oldIndex = i;
-
+	// Push pfp
+	const pushPfpFromUrl = async pfpUrl => {
 		try {
 			// Push pfp
-			const res = await axios.get(pfpsUrls[i], { responseType: "arraybuffer" });
+			const res = await axios.get(pfpUrl, { responseType: "arraybuffer" });
 			const result = await app.client.users.setPhoto({
 				token: process.env.SLACK_OAUTH_TOKEN,
 				image: Buffer.from(res.data),
@@ -40,12 +27,35 @@ const wiggler = async (app) => {
 		}
 	};
 
+	// Repeated func
+	const randomPushPfp = async isDay => {
+		let dayStr = isDay ? "day" : "night";
+
+		// Choose an index not equal to the last index (this algo makes it impossible to choose last pfp on first iteration)
+		// i iterates through pfpsUrls's indicies
+		let i = oldIndex + 1;
+		if (i >= pfpsUrlsAll[dayStr].length) {
+			i = 0;
+			// Shuffle pfpsUrls after going through all
+			pfpsUrlsAll[dayStr] = _.shuffle(pfpsUrlsAll[dayStr]);
+		}
+		oldIndex = i;
+
+		await pushPfpFromUrl(pfpsUrlsAll[dayStr][i]);
+	};
+
 	// Call every 10 seconds, but updates only every minute
 	const updateJob = setInterval(async () => {
-		let now = new Date();
+		let now = getLocalDateNow();
 		if (now.getMinutes() !== m) {
 			m = now.getMinutes();
-			await randomPushPfp();
+
+			if ((now.getHours() === 11 || now.getHours() === 23) && now.getMinutes() === 11) {
+				await pushPfpFromUrl(pfpsUrlsAll["special"]["1111"]);
+			} else {
+				// Day mode between 6 and 18 o'clock, otherwise night
+				await randomPushPfp(!!(now.getHours() >= 6 || now.getHours() <= 18));
+			}
 		}
 	}, 1000 * 10);
 };
